276cb7b4e427684bc13e9dd9411925e9929727df
subalakshmi <subageetha1999@gmail.com>
2024-05-28 07:56:17 +0530

    sample test

--- src/app/pages/addstaff/addstaff.component.ts
+++ src/app/pages/addstaff/addstaff.component.ts
@@ -1,36 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-addstaff',
   templateUrl: './addstaff.component.html',
   styleUrls: ['./addstaff.component.scss']
 })
 export class AddstaffComponent implements OnInit {
-
-  // staffForm: FormGroup;
+  staffForm: FormGroup;
 
   constructor(private router: Router, private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    // this.staffForm = this.fb.group({
-    //   staffName: ['', Validators.required],
-    //   username: ['', Validators.required],
-    //   Password: ['', Validators.required],
-    //   contactNumber: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
-    //   email: ['', [Validators.required, Validators.email]],
-    //   Role: ['', Validators.required],
-    //   location: ['', Validators.required]
-    // });
+    this.staffForm = this.fb.group({
+      Name: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      emailId: ['', Validators.required],
+      role: ['', Validators.required]
+    });
   }
 
-  // onSubmit() {
-  //   if (this.staffForm.valid) {
-  //     console.log('Form submitted:', this.staffForm.value);
-  //     this.router.navigate(['/staffspage']); // Change the route to the desired page after form submission
-  //   }
-  // }
-
- 
-
+  async onSubmit(): Promise<void> {
+    if (this.staffForm.valid) {
+      console.log(this.staffForm.value);
+      // Call your API or perform other actions here
+      this.router.navigate(['/dashboard']);
+    }
+  }
 }
